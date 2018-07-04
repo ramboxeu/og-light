8d63dfa0df0461d35d0f7a92c0305dfc44f2925b
RamboxEu <mail.rambox@gmail.com>
2018-07-04 14:21:09 +0200

    Migrating from TS to JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,7 @@
 const gulp = require('gulp');
-const ts = require('gulp-typescript');
 const connect = require(`gulp-connect`);
-const exec = require(`child_process`).exec;
 
-gulp.task(`default`, [`serve`, `compile`]);
-
-gulp.task(`compile`, () => {
-    return gulp.src(`src/**/*.ts`).pipe(ts({outFile: 'build.js'})).pipe(gulp.dest(`dist/`)).pipe(connect.reload());
-});
+gulp.task(`default`, [`serve`]);
 
 gulp.task(`serve`, () => {
     connect.server({
@@ -20,32 +14,10 @@ gulp.task(`html`, () => {
     gulp.src(`index.html`).pipe(gulp.dest('./')).pipe(connect.reload());
 });
 
-gulp.task(`css`, () => {
-    gulp.src(`./css/*`).pipe(gulp.dest('./css')).pipe(connect.reload());
-});
-
 gulp.task(`resources`, () => {
     gulp.src(`./resources/*`).pipe(gulp.dest('./resources')).pipe(connect.reload());
-});
-
-// Experimental version! Use it with responsibility
-gulp.task(`release`, () => {
-    if(process.argv[3] != null || process.argv[3] != undefined){
-        exec(`git checkout gh-pages`, () => {
-            exec(`git add -f dist/`, () => {
-                exec(`git commit`, () => {
-                    exec(`git tag -a ${process.argv[3]} -m`, () => {
-                        exec(`git push`)
-                    })
-                })
-            })
-        })
-    }
 });
 
 gulp.watch([`src/**/*.ts`], [`compile`]);
 gulp.watch([`./index.html`], [`html`]);
 gulp.watch([`./resources/*`], [`resources`]);
-gulp.watch([`./css/*`], [`css`]);
-gulp.watch([`img/*`], [`img`]);
-gulp.watch([`css/*`], [`css`]);
